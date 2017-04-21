0fabf5ae555a7b8a7d6aa1fc8ed411d314451847
ankur-gupta <ankur@perfectlyrandom.org>
2017-04-20 19:21:57 -0700

    Updating version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='dframe',
-      version='0.1.2',
-      description='Simple-to-user, index-less dataframe data structure in Python.',
+      version='0.1.3',
+      description='Easy-to-use, indexless dataframe data structure in Python.',
       url='http://github.com/ankur-gupta/dframe',
       author='Ankur Gupta',
       author_email='ankur@perfectlyrandom.org',

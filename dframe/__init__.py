8d342ba3c9f3c1a513675ff407ee22b5130c93d9
ankur-gupta <ankur@perfectlyrandom.org>
2017-04-23 13:10:29 -0700

    Updating __init__ and trying to codecov report transfer

--- dframe/__init__.py
+++ dframe/__init__.py
@@ -1,2 +1,3 @@
 from .dataframe import DataFrame
-from utils import is_scalar, is_string_type
+from .operations import hstack, vstack
+from utils import is_scalar, is_string_type, infer_dtype, to_best_dtype

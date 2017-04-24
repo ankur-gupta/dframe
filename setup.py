dca45dca440c2076eb754a88289c83b3187aa891
ankur-gupta <ankur@perfectlyrandom.org>
2017-04-23 23:25:28 -0700

    Bumping version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dframe',
-      version='0.1.3',
+      version='0.1.4',
       description='Easy-to-use, indexless dataframe data structure in Python.',
       classifiers=[
           'Development Status :: 3 - Alpha',
